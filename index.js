b064d79ac128ff1c9e016d81482b0468695678e7
Robin Nowakowski <bobinush@gmail.com>
2018-04-12 22:00:48 +0200

    post archiver v2.0.0 fix

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ function zipFolder(srcFolder, zipFilePath, callback) {
 	var output = fs.createWriteStream(zipFilePath);
 	var zipArchive = archiver('zip');
 
-	output.on('close', function() {
+	output.on('close', function () {
 		callback(null, zipArchive.pointer()); // sends back filesize in byte
 	});
 
 	zipArchive.pipe(output);
 
+	// pre 2.0.0
 	zipArchive.bulk([
 		{ cwd: srcFolder, src: ['**/*'], expand: true }
 	]);
 
-	zipArchive.finalize(function(err, bytes) {
-		if(err) {
+	// after 2.0.0
+	// zipArchive.directory(srcFolder, false);
+
+	zipArchive.finalize(function (err, bytes) {
+		if (err) {
 			callback(err, null);
 		}
 	});
